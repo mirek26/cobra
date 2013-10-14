5290283cbc0bf33cf1351825262426536e6091e3
Mirek Klimoš <myreggg@gmail.com>
2013-10-14 13:27:44 +0200

    for_all_combinations added to util.cpp

--- cobra/util.cpp
+++ cobra/util.cpp
@@ -3,9 +3,28 @@
  */
 #include <sstream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+#include "util.h"
 
 std::string to_string(int n) {
   std::ostringstream convert;
   convert << n;
   return convert.str();
+}
+
+template<class T>
+void for_all_combinations(int k, std::vector<T>& list, std::function<void(std::vector<T>)> action, int offset) {
+  static std::vector<T> combination;
+  if (k == 0) {
+    action(combination);
+    return;
+  }
+  for (int i = offset; i <= list.size() - k; ++i) {
+    combination.push_back(list[i]);
+    for_all_combinations(k-1, list, action, i+1);
+    combination.pop_back();
+  }
 }
--- cobra/util.h
+++ cobra/util.h
@@ -2,6 +2,9 @@
  * Copyright 2013, Mirek Klimos <myreggg@gmail.com>
  */
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
 #ifndef COBRA_UTIL_H_
 #define COBRA_UTIL_H_
@@ -10,5 +13,8 @@ typedef unsigned int uint;
 
 // convert int to string, equivalent to std::to_string (for llvm/clang)
 std::string to_string(int n);
+
+template<class T>
+void for_all_combinations(int k, std::vector<T>& list, std::function<void(std::vector<T>)> action, int offset = 0);
 
 #endif  // COBRA_UTIL_H_
